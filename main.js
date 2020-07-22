e2c2fdb52efd1ac15a6c0d0c5841fe0e1dc82c47
prem <prem.sherpa@codeimmersives.com>
2020-07-22 14:02:18 -0400

    more objects

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
 // Your code here.
 
 
+const getFirstName = function(obj){
+  return obj.firstName;
+}
 
+const getLastName = function(obj){
+  return obj.lastName;
+}
 
+const getFullName = function(obj){
+  return obj.firstName +' '+ obj.lastName;
+}
+
+const setFirstName = function(obj, newName){
+  obj.firstName = newName;
+}
+
+const setAge = function(obj, newAge){
+  obj.age = newAge;
+}
+
+const giveBirthday = function(obj){
+  //the first line
+  if ( obj.age){
+    obj.age++
+  }else {
+    obj.age = 1;
+  }
+}
+
+const marry = function(obj1, obj2){
+  obj1.married = obj2.married = true;
+
+  obj1.spouseName = getFullName(obj2);
+  obj2.spouseName = getFullName(obj1);
+
+}
+
+const divorce = function(obj1, obj2){
+  obj1.married = obj2.married = false;
+
+  delete obj1.spouseName;
+  delete obj2.spouseName;
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
